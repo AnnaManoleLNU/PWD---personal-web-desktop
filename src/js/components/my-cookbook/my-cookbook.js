b2ca28f7d4a1ebbca124419f788752746ac59a53
Anna Manole <am224wd@student.lnu.se>
2022-12-15 17:38:14 +0100

    connectedCB fetch recipe titles from API

--- src/js/components/my-cookbook/my-cookbook.js
+++ src/js/components/my-cookbook/my-cookbook.js
@@ -10,16 +10,18 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
 
-
-
 </style>
 
+<h1>Cookbook</h1>
+<p>What would you like to cook today?</p>
+<ul id="recipe-list"></ul>
 
 `
 
 customElements.define('my-cookbook',
 
     class extends HTMLElement {
+        #recipeList
 
         constructor() {
             super()
@@ -29,9 +31,23 @@ customElements.define('my-cookbook',
             this.attachShadow({ mode: 'open' })
                 .appendChild(template.content.cloneNode(true))
 
-
+            this.#recipeList = this.shadowRoot.querySelector('#recipe-list')
         }
 
+        async connectedCallback() {
+            const recipeList = this.#recipeList
+            
+            // Fetch 3 random recipes
+            for (let i = 0; i < 3; i++) {
+                // Get recipes from the API
+                const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+                const recipe = await response.json()
 
-    }
-)
+                const selectedRecipeTitle = recipe.meals[0].strMeal
+                const item = document.createElement('li')
+                item.textContent = selectedRecipeTitle      
+                recipeList.appendChild(item)
+            }
+        }
+
+    })
