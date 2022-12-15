7392062a84f1f61c86382801eaf69189bb2d352e
Anna Manole <am224wd@student.lnu.se>
2022-12-15 18:47:08 +0100

    Cookbook template, query selectors and generate instructions

--- src/js/components/my-cookbook/my-cookbook.js
+++ src/js/components/my-cookbook/my-cookbook.js
@@ -9,19 +9,41 @@
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
-
+    .hidden {
+        display:none;
+    }
 </style>
 
-<h1>Cookbook</h1>
-<p>What would you like to cook today?</p>
-<ul id="recipe-list"></ul>
+<div id="menu">
+    <h1>Cookbook</h1>
+    <h3>What would you like to cook today?</h3>
+    <div id="recipe-list"></div>
+</div>
+<div id="recipedetails" class="hidden">
+    <h1 id="title"></h1>
+    <ul id="ingredients"></ul>
+    <p id="instr"></p>
+    <button>Back to Cookbook</button>
+</div>
 
 `
 
 customElements.define('my-cookbook',
 
     class extends HTMLElement {
+        #menu
+
+        #recipeDetails
+
         #recipeList
+
+        #recipe
+
+        #recipeTitle
+
+        #instructions
+
+        #ingredients
 
         constructor() {
             super()
@@ -31,23 +53,46 @@ customElements.define('my-cookbook',
             this.attachShadow({ mode: 'open' })
                 .appendChild(template.content.cloneNode(true))
 
+            this.#menu = this.shadowRoot.querySelector('#menu')
+            this.#recipeDetails = this.shadowRoot.querySelector('#recipedetails')
             this.#recipeList = this.shadowRoot.querySelector('#recipe-list')
+            this.#recipeTitle = this.shadowRoot.querySelector('#title')
+            this.#instructions = this.shadowRoot.querySelector('#instr')
+        
         }
-
+        
         async connectedCallback() {
             const recipeList = this.#recipeList
-            
+
             // Fetch 3 random recipes
             for (let i = 0; i < 3; i++) {
                 // Get recipes from the API
                 const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
                 const recipe = await response.json()
-
+                
                 const selectedRecipeTitle = recipe.meals[0].strMeal
-                const item = document.createElement('li')
-                item.textContent = selectedRecipeTitle      
-                recipeList.appendChild(item)
+                this.#recipe = document.createElement('p')
+                this.#recipe.textContent = selectedRecipeTitle
+
+                // get all the ingredients and corresponding measurement
+                this.#ingredients = recipe.meals[0].strIngredient1
+
+                // get instructions
+                
+                this.#recipe.addEventListener('click', () => {
+                    console.log('generating instructions', selectedRecipeTitle)
+                    this.generateInstructions()
+                    this.#recipeTitle.textContent = selectedRecipeTitle
+                    this.#instructions.textContent = recipe.meals[0].strInstructions
+                })
+                
+                recipeList.appendChild(this.#recipe)
             }
         }
 
+        generateInstructions() {
+            this.#menu.setAttribute('class', 'hidden')
+            this.#recipeDetails.removeAttribute('class')
+        }
+        
     })
